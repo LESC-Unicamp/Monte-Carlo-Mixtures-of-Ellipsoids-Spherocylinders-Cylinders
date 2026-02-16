d85a93e6fbdc4c6a45893c1ee4b8e3a3eb0002e7
Nathan Barros de Souza <73966482+nathanbsouza@users.noreply.github.com>
2026-02-16 15:00:14 -0300

    Refactor HalfLength assignment for spherocylinders

--- src/module_overlap_algorithm.f95
+++ src/module_overlap_algorithm.f95
@@ -596,15 +596,13 @@ SquaredShortestDistance = ShortestDistance * ShortestDistance
 
 ! Half length of spherocylinder i
 IF( ASWPotentialCheck ) THEN
-  IF( GeometryType(2) ) HalfLength(1) = 0.5D0 * cLength(iComponent)
-  IF( GeometryType(3) ) HalfLength(1) = 0.5D0 * ( cLength(iComponent) + cPotentialRange(iComponent,pASWRange) )
+  HalfLength(1) = 0.5D0 * cLength(iComponent)
 ELSE IF( .NOT. ASWPotentialCheck ) THEN
   HalfLength(1) = 0.5D0 * cLength(iComponent)
 END IF
 ! Half length of spherocylinder j
 IF( ASWPotentialCheck ) THEN
-  IF( GeometryType(2) ) HalfLength(2) = 0.5D0 * cLength(jComponent)
-  IF( GeometryType(3) ) HalfLength(2) = 0.5D0 * ( cLength(jComponent) + cPotentialRange(jComponent,pASWRange) )
+  HalfLength(2) = 0.5D0 * cLength(jComponent)
 ELSE IF( .NOT. ASWPotentialCheck ) THEN
   HalfLength(2) = 0.5D0 * cLength(jComponent)
 END IF
